bcb067c4e4423a4ec6038c0b82287d31a0e54335
Gustavo P. Santana <gustavopsantana4@gmail.com>
2024-01-18 21:49:12 -0300

    Criando lógica de recuperação de senha

--- src/modules/sendMail.module.js
+++ src/modules/sendMail.module.js
@@ -3,8 +3,27 @@ const nodemailer = require('nodemailer');
 const sendMail = async (email, subject, text, html) => {
     try {
         const transporter = nodemailer.createTransport({
-            
-        })
+            host: 'smtp.office365.com',
+            port: 587,
+            secure: true,
+            auth: {
+                user: 'no-reply@ibvidaemcristo.com.br',
+                pass: 'editpro7@G'
+            }
+        });
+
+        await transporter.sendMail({
+            from: 'Mailer API',
+            to: email,
+            subject: subject,
+            text: text,
+            html: html
+        });
+
+        return {
+            status: 'success',
+            message: 'Message sent successfully'
+        }
     } catch (error) {
         return {
             status: 'error',
@@ -13,3 +32,5 @@ const sendMail = async (email, subject, text, html) => {
         }
     }
 }
+
+module.exports = sendMail;
