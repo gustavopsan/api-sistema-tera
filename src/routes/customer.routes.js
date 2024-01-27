9d3ee905fb5232478e2e6db3f583e70dcd86fba9
Gustavo P. Santana <gustavopsantana4@gmail.com>
2024-01-27 20:29:10 -0300

    Finalizando rotas de customers/clientes

--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const createCustomer = require('../controllers/customer/createCustomer.controller');
 const listCustomers = require('../controllers/customer/listCustomers.controller');
 const searchCustomer = require('../controllers/customer/searchCustomer.controller');
+const updateCustomer = require('../controllers/customer/updateCustomer.controller');
 
 router.post('/customer/create', (req, res) => {
     const { sellerId, name, address, docId, phone, route } = req.body;
@@ -32,6 +33,18 @@ router.post('/customer/search', (req, res) => {
     const {queryString, sellerId} = req.body;
 
     searchCustomer(queryString, sellerId)
+        .then(response => {
+            res.json(response);
+        })
+        .catch(err => {
+            res.json(err)
+        })
+})
+
+router.post('/customer/update', (req, res) => {
+    const { customerId, key, newvalue } = req.body;
+
+    updateCustomer(customerId, key, newvalue)
         .then(response => {
             res.json(response);
         })
