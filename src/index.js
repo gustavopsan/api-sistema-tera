cd8c410a788d4355ad36e2bfbbbffd81803f7a40
Gustavo P. Santana <gustavopsantana4@gmail.com>
2024-02-03 19:07:25 -0300

    Ajustando erro de cors

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, Accept, Origin");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
     app.use(CORS());
     next();
 })
