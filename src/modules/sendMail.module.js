0b96f6204eb44c9c43d595d016fb568ae616cf48
Gustavo P. Santana <gustavopsantana4@gmail.com>
2024-02-17 21:50:23 -0300

    Ajustando email de envio da API

--- src/modules/sendMail.module.js
+++ src/modules/sendMail.module.js
@@ -13,7 +13,7 @@ const sendMail = async (email, subject, text, html) => {
         });
 
         await transporter.sendMail({
-            from: `Mailer API ${process.env.MAILER_USERNAME}`,
+            from: `Tera Mailer ${process.env.MAILER_USERNAME}`,
             to: email,
             subject: subject,
             text: text,
