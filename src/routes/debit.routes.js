899f59ec70853363d3f6f3d7be438038070dd881
Gustavo P. Santana <gustavopsantana4@gmail.com>
2024-03-02 18:00:59 -0300

    Busca de débitos

--- src/routes/debit.routes.js
+++ src/routes/debit.routes.js
@@ -4,6 +4,7 @@ const createDebit = require('../controllers/debit/createDebit.controller');
 const listDebits = require('../controllers/debit/listDebits.controller');
 const payDebit = require('../controllers/debit/payDebit.controller');
 const getDebit = require('../controllers/debit/getDebit.controller');
+const searchDebit = require('../controllers/debit/searchDebit.controller');
 
 router.post('/debits/create', (req, res) => {
     const { sellerId, customerId, value, paymentsAmount, paymentsRemaing } = req.body;
@@ -27,6 +28,18 @@ router.post('/debits/pay', (req, res) => {
     payDebit(debitId, paidValue)
         .then(response => res.json(response))
         .catch(error => res.json(error))
+})
+
+router.post('/debits/search', (req, res) => {
+    const { queryString, sellerId } = req.body;
+
+    searchDebit(queryString, sellerId)
+        .then(response => {
+            res.json(response);
+        })
+        .catch(err => {
+            res.json(err)
+        })
 })
 
 router.post('/debits/find', (req, res) => {
