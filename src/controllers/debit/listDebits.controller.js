b08cab535599b6483fde56253d6a48c2a3d0299d
Gustavo P. Santana <gustavopsantana4@gmail.com>
2024-03-02 23:18:49 -0300

    Ajustando retorno de crediários

--- src/controllers/debit/listDebits.controller.js
+++ src/controllers/debit/listDebits.controller.js
@@ -2,7 +2,7 @@ const debitModel = require("../../models/debit.model");
 
 async function listDebits(sellerId) {
     try {
-        const debits = await debitModel.find({sellerId: sellerId});
+        const debits = await debitModel.find({sellerId: sellerId, isQuited: false});
 
         return debits;
     } catch (error) {
