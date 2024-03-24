9fca303305d4d3fe9aa0ff493785f30a82021a31
Gustavo P. Santana <gustavo@iMac-de-Gustavo.lan>
2024-03-23 17:37:24 -0700

    Ajustando iteração de dados gerais dos crediários

--- src/controllers/loadGeneralData.controller.js
+++ src/controllers/loadGeneralData.controller.js
@@ -1,16 +1,5 @@
 const debitModel = require("../models/debit.model");
-
-Date.prototype.getWeek = function() {
-    var date = new Date(this.getTime());
-    date.setHours(0, 0, 0, 0);
-    // Thursday in current week decides the year.
-    date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
-    // January 4 is always in week 1.
-    var week1 = new Date(date.getFullYear(), 0, 4);
-    // Adjust to Thursday in week 1 and count number of weeks from date to week1.
-    return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
-                          - 3 + (week1.getDay() + 6) % 7) / 7);
-  }
+const dateCraft = require('date-craft');
 
 async function loadGeneralData(sellerId) {
     try {
@@ -18,56 +7,63 @@ async function loadGeneralData(sellerId) {
         var paymentsToday = 0;
         var paymentsThisWeek = 0;
         var creditsToday = 0;
-        var creditsthisWeek = 0;
+        var creditsThisWeek = 0;
         var paymentsThisMonth = 0;
-        var creditsthisMonth = 0;
+        var creditsThisMonth = 0;
 
-        var actualDate = new Date();
-        actualDate.setHours(actualDate.getHours() - 7);
+        var newActualDate = dateCraft.getCurrentDate();
+        newActualDate.setHours(newActualDate.getHours() - 7);
+        var startOfWeek = dateCraft.getStartOfWeek(newActualDate);
+        var endOfWeek = dateCraft.getEndOfWeek(newActualDate);
 
         debits.forEach(debit => {
-
-            var createdToday = new Date(debit.firstPaymentDate).toISOString().split('T')[0] === actualDate.toISOString().split('T')[0];
-            var createdThisWeek = new Date(debit.firstPaymentDate).getWeek() === new Date().getWeek();
+            var createdToday = debit.firstPaymentDate.split('T')[0] == newActualDate.toISOString().split('T')[0];
+            var createdThisWeek = debit.firstPaymentDate.split('T')[0] >= startOfWeek.toISOString().split('T')[0] && debit.firstPaymentDate.split('T')[0] <= endOfWeek.toISOString().split('T')[0];
+            var createdThisMonth = new Date(debit.firstPaymentDate).getMonth() == newActualDate.getMonth();
 
             if (createdToday) {
-                creditsToday = creditsToday + parseFloat(debit.totalValue);
+                creditsToday = parseFloat(creditsToday) + parseFloat(debit.totalValue);
             }
 
             if (createdThisWeek) {
-                creditsthisWeek = creditsthisWeek + parseFloat(debit.totalValue);
+                creditsThisWeek = parseFloat(creditsThisWeek) + parseFloat(debit.totalValue);
             }
 
-            if (new Date(debit.firstPaymentDate).getMonth() === new Date().getMonth()) {
-                creditsthisMonth = creditsthisMonth + parseFloat(debit.totalValue);
+            if (createdThisMonth) {
+                creditsThisMonth = parseFloat(creditsThisMonth) + parseFloat(debit.totalValue);
             }
 
             debit.payments.forEach(payment => {
-                if (new Date(payment[0].date).toString().split('T')[0] === new Date().toString().split('T')[0]) { 
-                    paymentsToday = paymentsToday + parseFloat(payment[0].value);
+                var paidToday = payment[0].date.split('T')[0] == newActualDate.toISOString().split('T')[0];
+                var paidThisWeek = payment[0].date.split('T')[0] >= startOfWeek.toISOString().split('T')[0] && payment[0].date.split('T')[0] <= endOfWeek.toISOString().split('T')[0];
+                var paidThisMonth = new Date(payment[0].date).getMonth() == newActualDate.getMonth();
+
+                if (paidToday) {
+                    paymentsToday = parseFloat(paymentsToday) + parseFloat(payment[0].value);
                 }
 
-                if (new Date(payment[0].date).getWeek() === new Date().getWeek()) {
-                    paymentsThisWeek = paymentsThisWeek + parseFloat(payment[0].value);
+                if (paidThisWeek) {
+                    paymentsThisWeek = parseFloat(paymentsThisWeek) + parseFloat(payment[0].value);
                 }
 
-                if (new Date(payment[0].date).getMonth() === new Date().getMonth()) {
-                    paymentsThisMonth = paymentsThisMonth + parseFloat(payment[0].value);
+                if (paidThisMonth) {
+                    paymentsThisMonth = parseFloat(paymentsThisMonth) + parseFloat(payment[0].value);
                 }
-
             })
+
             
         })
 
         return {
             paymentsToday: paymentsToday,
             paymentsThisWeek: paymentsThisWeek,
-            creditsToday: creditsToday,
-            creditsthisWeek: creditsthisWeek,
             paymentsThisMonth: paymentsThisMonth,
-            creditsthisMonth: creditsthisMonth
+            creditsToday: creditsToday,
+            creditsThisWeek: creditsThisWeek,
+            creditsThisMonth: creditsThisMonth
         }
-    } catch (error) {
+
+    } catch(error) {
       return error
     }
 }
