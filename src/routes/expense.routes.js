33be788856d4bd597a4c64829fc054ae888acf6d
Gustavo P. Santana <gustavo@iMac-de-Gustavo.lan>
2024-04-08 20:53:56 -0700

    Filtrando despesas

--- src/routes/expense.routes.js
+++ src/routes/expense.routes.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const createExpense = require('../controllers/expenses/createExpense.controller');
 const listExpenses = require('../controllers/expenses/listExpenses.controller');
 const removeExpense = require('../controllers/expenses/removeExpense.controller');
+const filterExpenses = require('../controllers/expenses/filterExpenses.controller');
 
 router.post('/expense/create', (req, res) => {
     const { description, date, value, sellerId } = req.body;
@@ -32,6 +33,18 @@ router.post('/expense/remove', (req, res) => {
     const { expenseId } = req.body;
 
     removeExpense(expenseId)
+        .then(response => {
+            res.json(response);
+        })
+        .catch(err => {
+            res.json(err)
+        })
+})
+
+router.post('/expense/filter', (req, res) => {
+    const { sellerId, initialDate, finalDate } = req.body;
+
+    filterExpenses(sellerId, initialDate, finalDate)
         .then(response => {
             res.json(response);
         })
