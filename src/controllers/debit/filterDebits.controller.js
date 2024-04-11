35b2973dfcd3527b1bf01d85c780e5bd5dd1d798
Gustavo P. Santana <gustavo@iMac-de-Gustavo.lan>
2024-04-10 22:42:38 -0700

    Ajustando filtro

--- src/controllers/debit/filterDebits.controller.js
+++ src/controllers/debit/filterDebits.controller.js
@@ -12,9 +12,8 @@ async function filterDebits(sellerId, initialDate, finalDate) {
         debits.forEach(debit => {
             debit.payments.forEach(payment => {
                 var paymentDate = new Date(payment[0].date);
-                paymentDate.setHours(paymentDate.getHours() - 3);
 
-                if (paymentDate => iniDate && paymentDate <= finDate) {
+                if (paymentDate >= iniDate && paymentDate <= finDate) {
                     totalValue = parseFloat(totalValue) + parseFloat(payment[0].value);
 
                     var paymentInfo = {
