c7da80dc0964d7805f48148e611b7369a7368766
Gustavo P. Santana <gustavopsantana4@gmail.com>
2024-05-04 17:25:07 -0300

    Debugando

--- src/controllers/guarantee/payments/paymentsThisMonth.controller.js
+++ src/controllers/guarantee/payments/paymentsThisMonth.controller.js
@@ -14,10 +14,7 @@ async function paymentsThisMonth(sellerId) {
                 var paidThisMonth = new Date(payment[0].date).getMonth() == newActualDate.getMonth();
 
                 if (paidThisMonth) {
-                    console.log({
-                        debitId: debit._id,
-                        date: payment[0].date
-                    });
+                    console.log(payment[0].date);
                     paymentsThisMonth = parseFloat(paymentsThisMonth) + parseFloat(payment[0].value);
                 }
             })
