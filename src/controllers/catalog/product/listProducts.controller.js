9ba29a833cc745e67d75738d493fd529ae588c4b
Gustavo P. Santana <gustavopsantana4@gmail.com>
2024-05-12 22:50:43 -0300

    Ajustando listagem de produtos

--- src/controllers/catalog/product/listProducts.controller.js
+++ src/controllers/catalog/product/listProducts.controller.js
@@ -2,7 +2,7 @@ const productModel = require('../../../models/product.model');
 
 async function listProducts(sellerId) {
     try {
-        const products = await productModel.find({ sellerId: sellerId, isActive: true });
+        const products = await productModel.find({ sellerId: sellerId });
 
         return products
     } catch (error) {
