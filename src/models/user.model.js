ee73da0fc6fe36c04ba52b526ed7a3aac25f2bf2
Gustavo P. Santana <gustavopsantana4@gmail.com>
2024-08-31 12:31:50 -0300

    .

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -18,11 +18,28 @@ const userSchema = new mongoose.Schema(
             type: "String",
             unique: true
         },
+        firstBalance: 'Decimal',
         password: 'String',
         isActive: 'Boolean'
     },
     { timestamps: true }
 );
+
+const decimal2JSON = (v, i, prev) => {
+    if (v !== null && typeof v === 'object') {
+       if (v.constructor.name === 'Decimal128')
+          prev[i] = parseFloat(v);
+       else
+          Object.entries(v).forEach(([key, value]) => decimal2JSON(value, key, prev ? prev[i] : v));
+    }
+};
+
+userSchema.set('toJSON', {
+    transform: (doc, ret) => {
+       decimal2JSON(ret);
+       return ret;
+    }
+});
 
 const User = mongoose.model('User', userSchema);
 
